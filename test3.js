732e1ed91836f3eb2ea186359f1ce7968a4d6db3
PolinaK4 <p.kaliuzhnaya@gmail.com>
2022-09-25 22:13:34 +0200

    lesson 4

--- test3.js
+++ test3.js
@@ -1,1 +1,15 @@
-hellowww
+const fse = require ('fs-extra');
+
+fse.ensureDirSync('./newfolder');
+fse.ensureFileSync('./newfolder/for-add.txt');
+fse.ensureDirSync('./folder2');
+fse.moveSync('./newfolder/for-add.txt', './folder2/for-add.txt');
+g
+fse.ensureDirSync('./folder3');
+fse.copySync('./folder2/for-add.txt', './folder3/for-add.txt');
+
+fse.removeSync('./folder2/for-add.txt');
+fse.removeSync('./folder3/for-add.txt');
+fse.removeSync('./folder2');
+fse.removeSync('./folder3');
+fse.removeSync('./newfolder');
